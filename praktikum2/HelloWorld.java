35cd46ce320e9ad17f48c945ea5469bb9dc8c929
dimas235 <dimasprayuda80@gmail.com>
2021-09-27 17:15:16 +0700

    HelloWorld.java

---
+++ praktikum2/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+
+	public static void main (String[] args) {
+		System.out.println ("Nama : Dimas Prayuda");
+		System.out.println ("NIM : 6706213080");
+		System.out.println ("Kelas : 45-03");
+	}
+}
